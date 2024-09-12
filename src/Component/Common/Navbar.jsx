2cb4b1836957cfae5a51505df903c878f89cfe2b
VINAYAK SHARMA <mac@Vinayaks-Macintosh.local>
2024-09-12 22:15:50 +0530

    Add existing project files to Git

---
+++ src/Component/Common/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Highlightedtext from './Highlightedtext'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            SMM <Highlightedtext text='Panel' />
+        </div>  
+    </div>
+  )
+}
+
+export default Navbar
