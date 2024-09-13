bbb2702c1b581bdd285d678a57f35fd7a324bee1
YashKaushik7824 <vagarthvermavv15@gmail.com>
2024-09-13 19:17:57 +0530

    Commit

--- src/Component/pages/Home.jsx
+++ src/Component/pages/Home.jsx
@@ -2,6 +2,9 @@ import './Home.css';
 import React from 'react'
 import Navbar from '../Common/Navbar'
 import { CiSearch } from "react-icons/ci";
+import { MdEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 import Highlightedtect from '../Common/Highlightedtect';
 
 const Home = () => {
@@ -17,6 +20,57 @@ const Home = () => {
                 <CiSearch size={24}/>
               </div>
               <div className='bg-[#3D55CC] py-[12px] px-[24px] rounded-[48px]'>Search</div>
+            </div>
+          </div>
+          <div className='flex items-center justify-center h-[663px] w-[746px] rounded-[24px] gap-10 bg-[#F6F6F6] shadow-[0px_8px_48px_0px_#00000014]'>
+            <div className='flex flex-col w-[587px] h-[591px] gap-[36px] justify-center items-center'>
+              <div className='flex flex-col w-[335px] h-[59px] gap-[8px]'>
+                <p className='font-semibold text-[24px] leading-[29.05px] text-center'>Login to <Highlightedtect text={'SMM panel'}/></p>
+                <p className='text-[#828282] text-[18px] font-[400] leading-[21.78px]'>Welcome back! Please enter your detail </p>
+              </div>
+              <div className='flex flex-col items-center justify-center w-[587px] h-[296px] gap-[16px]'>
+                <div className='w-[587px] h-[296px] gap-[36px]'>
+                  <div className='h-[78px] flex flex-col justify-between gap-[8px]'>
+                    <p className='text-[#4B4B4B] font-[500] text-[18px] leading-[21.78px]'>Email-ID</p>
+                    <div className='flex bg-[#F8F8F8] h-[48px] border-[#BBBBBB] border-[1px] w-full items-center py-[12px] px-[24px] gap-10 rounded-[48px]'>
+                      <MdEmail className='text-blue-600' size={24}/>
+                      <input className='bg-[#F8F8F8] text-[#828282] font-[400] text-[18px] leading-[21.78px]' placeholder='Enter your Email'></input>
+                    </div>
+                  </div>
+                  <div className='h-[78px] flex flex-col justify-between gap-[8px]'>
+                    <p className='text-[#4B4B4B] font-[500] text-[18px] leading-[21.78px]'>Password</p>
+                    <div className='flex bg-[#F8F8F8] justify-between h-[48px] border-[#BBBBBB] border-[1px] w-full items-center py-[12px] px-[24px] gap-10 rounded-[48px]'>
+                      <div className='flex gap-10'>
+                        <FaLock className='text-blue-600' size={20}/>
+                        <input className='bg-[#F8F8F8] text-[#828282] font-[400] text-[18px] leading-[21.78px]' placeholder='Enter password'/>
+                      </div>
+                      <FaRegEyeSlash />
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                    <input type='checkbox'/>
+                    <p>Remember me</p>
+                    </div>
+                    <p>Forget Password ?</p>
+                  </div>
+                  <div>Login</div>
+                  <div>
+                    <hr/>
+                    <p>or</p>
+                    <hr/>
+                  </div>
+                  <div>
+                    <div>
+                      <p>Login with Google</p>
+                    </div>
+                    <div>
+                      <p>Login with Facebook</p>
+                    </div>
+                  </div>
+                  <p>Don’t have an account? <Highlightedtect text={'Sign up'}/></p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
