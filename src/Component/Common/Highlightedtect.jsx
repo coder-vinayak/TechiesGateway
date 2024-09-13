82971dac7272da3f8a65301d3d1c2e94e0d32e56
YashKaushik7824 <vagarthvermavv15@gmail.com>
2024-09-13 11:45:19 +0530

    New commit

---
+++ src/Component/Common/Highlightedtect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Highlightedtect = ({text}) => {
+  return (
+    <span className='text-[#3D55CC] font-semibold text-center max-w-fit'>
+        {text}
+    </span>
+  )
+}
+
+export default Highlightedtect
