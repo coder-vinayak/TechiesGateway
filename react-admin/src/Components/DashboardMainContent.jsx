bf36a84530f15c9347c8a5d289817302ea08172f
YashKaushik7824 <vagarthvermavv15@gmail.com>
2024-10-12 22:14:57 +0530

    welcom chakiye

--- react-admin/src/Components/DashboardMainContent.jsx
+++ react-admin/src/Components/DashboardMainContent.jsx
@@ -1,82 +1,52 @@
 // DashboardMainContent.js
 import React from 'react';
-import Card from './Cardsofdashboard';
-
+import Cardsofdashboard from './Cardsofdashboard';
+import { FaDollarSign } from "react-icons/fa6";
+import './Cardsofdashboard.css'
 const DashboardMainContent = () => {
   return (
-    <div className="container-fluid p-4 ">
-      <div className="row">
-        <Card
-          icon="feather-dollar-sign"
-          count={45}
-          total={76}
-          title="Invoices Awaiting Payment"
-          amount="$5,569"
-          percentage={56}
-          progressColor="bg-primary"
+    <div className="container-fluid p-4">
+      <div className="row2 row">
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
-        <Card
-          icon="feather-cast"
-          count={48}
-          total={86}
-          title="Converted Leads"
-          amount="52 Completed"
-          percentage={63}
-          progressColor="bg-warning"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
-        <Card
-          icon="feather-briefcase"
-          count={16}
-          total={20}
-          title="Projects In Progress"
-          amount="16 Completed"
-          percentage={78}
-          progressColor="bg-success"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
+          
         />
-        <Card
-          icon="feather-activity"
-          count={46.59}
-          total={100}
-          title="Conversion Rate"
-          amount="$2,254"
-          percentage={46}
-          progressColor="bg-danger"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
-           <Card
-          icon="feather-activity"
-          count={46.59}
-          total={100}
-          title="Conversion Rate"
-          amount="$2,254"
-          percentage={46}
-          progressColor="bg-danger"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
-           <Card
-          icon="feather-activity"
-          count={46.59}
-          total={100}
-          title="Conversion Rate"
-          amount="$2,254"
-          percentage={46}
-          progressColor="bg-danger"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
-           <Card
-          icon="feather-activity"
-          count={46.59}
-          total={100}
-          title="Conversion Rate"
-          amount="$2,254"
-          percentage={46}
-          progressColor="bg-danger"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
-           <Card
-          icon="feather-activity"
-          count={46.59}
-          total={100}
-          title="Conversion Rate"
-          amount="$2,254"
-          percentage={46}
-          progressColor="bg-danger"
+        <Cardsofdashboard
+          amt="40,000"
+          title="Total users"
+          icon={<FaDollarSign/>}
         />
       </div>
     </div>
